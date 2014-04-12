5c2329139335c6b7b104b31aa4d232b323b14c10
vvorski <victor@vorski.com>
2014-04-12 12:06:24 +0100

    info on where to get parse.com application key

--- settings/settings_example.js
+++ settings/settings_example.js
@@ -3,8 +3,8 @@
  */
 function settings(_exports) {
   exports = _exports || exports;
-  exports.ParseApplicationId = "<insert parse.com application id key>"; // see
-  exports.ParsejavaScriptKey = "<insert parse.com javascript script key>"; // see
+  exports.ParseApplicationId = "<insert parse.com application id key>"; // see "Application Keys" in https://www.parse.com/apps/ for your test app on parse.com
+  exports.ParsejavaScriptKey = "<insert parse.com javascript script key>"; // see "Application Keys" in https://www.parse.com/apps/ for your test app on parse.com
   exports.FBappId = "<insert facebook application id>"; //
   exports.debug = true; // see README.md for usage
 }
